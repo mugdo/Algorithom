fa3ac6734bace2103592aecc23adfbc998964c0d
Shaiful Islam <mug.shaiful97@gmail.com>
2023-01-17 13:13:01 +0600

    Add files via upload

---
+++ inline_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline int max_number(long long int a, long long int b){
+    return (a>b)? a : b;
+}
+int main(){
+
+    cout<<max_number(10000,100)<<endl;
+
+    return 0;
+}
